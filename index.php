2481dd1d60443a3c0f840ee7b2a94cf255114881
MohamedBarhmate <barhmate.mohamed@gmail.com>
2023-09-27 17:59:10 -0400

    add form and result page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="resultat.php" methode="post">
+        Name:
+        <input type="text" name="fname">
+        <input type="submit">
+    </form>
+</body>
+</html>
